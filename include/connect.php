d70d7436774f6ccec64a2ef26f1f95c3f6b0fbab
MorganeLM <morganelemarquer@lilo.org>
2020-07-31 22:28:57 +0200

    mis en place du php

---
+++ include/connect.php
@@ -0,0 +1,18 @@
+<?php
+// On se connecte à la base de données
+try{
+    // On essaie de se connecter
+    $dsn = 'mysql:dbname=projet_asso;host=localhost';
+
+    // DSN, Utilisateur, Mot de passe
+    $db = new PDO($dsn, 'root', '');
+
+    // echo 'La connextion a fonctionné.';
+
+}catch(Exception $erreur){
+    // On gère l'échec du "try"
+    echo 'La connexion a échoué';
+    die;
+}
+
+?>
